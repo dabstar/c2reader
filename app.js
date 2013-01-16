b338e956cbb19ba564d32295958a4cb914d8ae4a
dabstar <dabstar@gmail.com>
2013-01-16 16:52:35 +0100

    init

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
+  , about = require('./routes/about')
   , http = require('http')
   , path = require('path');
 
@@ -31,7 +32,10 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/about', about.about);
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
+
